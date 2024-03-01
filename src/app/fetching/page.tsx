99fd7b80cbf2e538fdd09d6b208b06bb9e96d71d
Sander Cokart <cokart32@gmail.com>
2024-03-01 16:08:04 +0100

    Refactor page layout using ul, li, and flexbox properties.
    The changes include wrapping each topic in a li element instead of directly linking to them. This makes the HTML semantics clearer. In addition, it utilizes flexbox properties to ensure the 'Work in progress' text is always positioned at the bottom of the card, regardless of the content in the card title or description. Flex grow is used on the CardHeader component to allow it to take up any remaining space.

--- src/app/fetching/page.tsx
+++ src/app/fetching/page.tsx
@@ -40,21 +40,23 @@ export default async function FetchingPage() {
 
   return (
     <main>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-8 p-8">
+      <ul className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-8 p-8">
         {topics.map(({ topic, description, workInProgress, href }) => (
-          <Link key={topic} className={cn(workInProgress && 'cursor-not-allowed')} href={workInProgress ? '' : href}>
-            <Card className="hover:bg-accent hover:text-accent-foreground">
-              <CardHeader>
-                <CardTitle>{topic}</CardTitle>
-                <CardDescription>{description}</CardDescription>
-              </CardHeader>
-              <CardFooter>
-                {workInProgress && <span className="text-sm text-destructive">Work in progress</span>}
-              </CardFooter>
-            </Card>
-          </Link>
+          <li key={topic}>
+            <Link className={cn(workInProgress && 'cursor-not-allowed')} href={workInProgress ? '' : href}>
+              <Card className="flex h-full flex-col hover:bg-accent hover:text-accent-foreground">
+                <CardHeader className="grow">
+                  <CardTitle>{topic}</CardTitle>
+                  <CardDescription>{description}</CardDescription>
+                </CardHeader>
+                <CardFooter>
+                  {workInProgress && <span className="text-sm text-destructive">Work in progress</span>}
+                </CardFooter>
+              </Card>
+            </Link>
+          </li>
         ))}
-      </div>
+      </ul>
     </main>
   );
 }
